a40cd894ba91f1e41b177eaaca4f1d7b82d35acc
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-04 15:35:07 +0200

    ahmet degisiklik 2

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Github Kisiel Kullanim");
+        System.out.println("Ahmet in branchi ile main " +
+                "arasinda merge yaptik");
     }
 }
