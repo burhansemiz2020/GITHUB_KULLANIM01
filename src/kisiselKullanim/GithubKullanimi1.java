96110164b198b08a517a81e617869a9e9a9189d4
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-04 15:37:36 +0200

    main branch version1

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Github Kisiel Kullanim");
+        System.out.println("Main branch degisiklik 1. satir");
+        System.out.println("Main branch degisiklik 2. satir");
     }
 }
